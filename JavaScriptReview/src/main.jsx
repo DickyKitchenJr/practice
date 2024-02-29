eb945d4b4afe5437922d5557a7df5ce481c12fa5
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-02-29 13:58:12 -0500

    boilerplate Vite React project

---
+++ JavaScriptReview/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import JSReview from './JSReview.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JSReview />
+  </React.StrictMode>,
+)
