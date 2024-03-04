e77d9cd5fe4d9959588c182a2e3802bf52f828bf
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-04 10:32:23 -0500

    Update App.jsx
    completed assessment challenge

--- CodingAssessment/src/App.jsx
+++ CodingAssessment/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 const API_URL = "https://jsonplaceholder.typicode.com/users";
 
@@ -11,16 +11,82 @@ const API_URL = "https://jsonplaceholder.typicode.com/users";
  */
 
 function App() {
+  const [initialData, setInitialData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const returnedData = await fetch(API_URL);
+        const processedData = await returnedData.json();
+        console.log(processedData);
+        setInitialData(processedData);
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleFilteredData = (e) => {
+    const input = e.target.value;
+    const entry = [];
+    for (let i = 0; i < initialData.length; i++) {
+      console.log(i);
+      console.log(initialData.length);
+      const inputUpper = input.toUpperCase();
+      const nameUpper = initialData[i].name.toUpperCase();
+      const emailUpper = initialData[i].email.toUpperCase();
+      if (nameUpper.includes(inputUpper) || emailUpper.includes(inputUpper)) {
+        entry.push(initialData[i]);
+      }
+    }
+    setFilteredData(entry);
+  };
 
   return (
     <>
       <main>
-        <div className='div1'>
-          <h1>Candidate Assessment...</h1>
+        <div className="div1">
+          <h1>Users:</h1>
+          <br />
+          <label htmlFor="search">Search</label>
+          <input type="text" name="search" onChange={handleFilteredData} />
+          <br />
+          {initialData.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {filteredData.length !== 0 ? (
+                <>
+                  {filteredData.map((user) => (
+                    <>
+                      <p>User: {user.name}</p>
+                      <p>Email: {user.email}</p>
+                      <a href={`https://${user.website}`}>{user.website}</a>
+                      <br />
+                    </>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {initialData.map((user) => (
+                    <>
+                      <p>User: {user.name}</p>
+                      <p>Email: {user.email}</p>
+                      <a href={`https://${user.website}`}>{user.website}</a>
+                      <br />
+                    </>
+                  ))}{" "}
+                </>
+              )}
+            </>
+          )}
         </div>
       </main>
     </>
   );
 }
 
-export default App
+export default App;
