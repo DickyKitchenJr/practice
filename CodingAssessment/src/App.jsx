6729d2593b8942838532e1260d479a1ee7e76077
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-04 09:29:45 -0500

    boilerplate plus starting code for assessment

---
+++ CodingAssessment/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+/**
+ * List all users
+ * display name, email and website as clickable link
+ *
+ * add filtering by name and email that is case insensitive
+ */
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className='div1'>
+          <h1>Candidate Assessment...</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App
