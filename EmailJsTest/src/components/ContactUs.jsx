b75e955d12a744027dfcc2acb6b929b4383d4b16
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-14 11:17:15 -0400

    Update ContactUs.jsx

--- EmailJsTest/src/components/ContactUs.jsx
+++ EmailJsTest/src/components/ContactUs.jsx
@@ -1,16 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 export const ContactUs = () => {
   const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const sendEmail = (e) => {
+  const handleNameChange = (e) => {
+    let input = e.target.value;
+
+    setName(input);
+  };
+
+  const handleEmailChange = (e) => {
+    let input = e.target.value;
+
+    setEmail(input);
+  };
+
+  const handleMessageChange = (e) => {
+    let input = e.target.value;
+
+    setMessage(input);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    //.sendForm("service id", "template id", form.current, {publicKey: "public key"})
     emailjs
-      .sendForm("contact_service", "template_18nwxbn", form.current, {
-        publicKey: "iX7305v5_CCdTLK2r",
+      .sendForm("service id", "template id", form.current, {
+        publicKey: "public key",
       })
       .then(
         () => {
@@ -23,13 +43,30 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+    <form ref={form} onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input type="text" name="user_name" id="name" />
+      <input
+        type="text"
+        name="user_name"
+        id="name"
+        value={name}
+        onChange={handleNameChange}
+      />
       <label htmlFor="email">Email</label>
-      <input type="email" name="user_email" id="email" />
+      <input
+        type="email"
+        name="user_email"
+        id="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
       <label htmlFor="message">Message</label>
-      <textarea name="message" id="message" />
+      <textarea
+        name="message"
+        id="message"
+        value={message}
+        onChange={handleMessageChange}
+      />
       <input type="submit" value="Send" />
     </form>
   );
