034b603d5daebbccac5ee86137caae9072e63980
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-23 13:37:23 -0400

    Update Greeting.test.jsx

--- Vitest/src/tests/Greeting.test.jsx
+++ Vitest/src/tests/Greeting.test.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest"
+import "@testing-library/jest-dom/vitest";
 import Greeting from "../components/Greeting";
 
 describe("Greeting", () => {
   it("should render Hello with the name if name is true", () => {
     render(<Greeting name="Dicky" />);
-    const heading = screen.getByRole('heading');
+    const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/dicky/i);
+  });
+
+  it("should render a button if name is false", () => {
+    render(<Greeting />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
   });
 });
