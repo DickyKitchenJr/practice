d5de5208a88fd1249aff4ed626ff4fc5154b9262
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-23 14:40:05 -0400

    imported library into setup file

--- Vitest/src/tests/Greeting.test.jsx
+++ Vitest/src/tests/Greeting.test.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
 import Greeting from "../components/Greeting";
 
 describe("Greeting", () => {
