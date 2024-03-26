e582ebb3256a7a1fec84293da699a1852f4ebaa2
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-26 13:44:32 -0400

    Create Home.jsx

---
+++ Vitest/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <>
+        <header>
+            <h1>Home Page</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>Blog</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <p>This is the main content</p>
+        </main>
+        <footer>
+            <p>This is the footer</p>
+        </footer>
+        </>
+    )
+}
+
+export default Home;
