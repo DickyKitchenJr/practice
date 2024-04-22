a4c27ef6831751dedf1661fcc95e714be2e99233
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-04-22 12:03:54 -0400

    Update index.js

--- TypeScriptPractice/dst/index.js
+++ TypeScriptPractice/dst/index.js
@@ -2,3 +2,11 @@
 let age = 43;
 let firstName = "Dicky";
 let talented = true;
+let timeToRetire = false;
+if (age <= 67) {
+    timeToRetire = false;
+}
+else {
+    timeToRetire = true;
+}
+//# sourceMappingURL=index.js.map
