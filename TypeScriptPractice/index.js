65f5ce87f7c59b35950fc5e5d0a5433210d80908
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-04-22 10:40:40 -0400

    fooling around with typescript
    practicing the fundamentals of TypeScript

---
+++ TypeScriptPractice/index.js
@@ -0,0 +1,4 @@
+//examples of various types
+var age = 43;
+var firstName = "Dicky";
+var talented = true;
---
+++ TypeScriptPractice/index.ts
@@ -0,0 +1,7 @@
+//examples of various types
+
+let age: number = 43;
+
+let firstName: string = "Dicky";
+
+let talented: boolean = true;
