7979db47f724567f7dcd37e38efdae395030e0a0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-04-22 12:03:51 -0400

    Update index.ts

--- TypeScriptPractice/src/index.ts
+++ TypeScriptPractice/src/index.ts
@@ -5,3 +5,11 @@ let age: number = 43;
 let firstName: string = "Dicky";
 
 let talented: boolean = true;
+
+let timeToRetire: boolean = false;
+
+if (age <= 67) {
+  timeToRetire = false;
+} else {
+  timeToRetire = true;
+}
