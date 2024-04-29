20d29f28e3799cab94037c1d4129b389fbd9dc41
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-04-29 09:26:28 -0400

    Update index.js

--- TypeScriptPractice/dst/index.js
+++ TypeScriptPractice/dst/index.js
@@ -2,6 +2,7 @@
 let age = 43;
 let firstName = "Dicky";
 let talented = true;
+let skillsArray = ["React", "JavaScript", "TypeScript"];
 let timeToRetire = false;
 if (age <= 67) {
     timeToRetire = false;
@@ -9,4 +10,18 @@ if (age <= 67) {
 else {
     timeToRetire = true;
 }
+let moneyNeededToRetire = 4000000;
+let five = 5;
+let mystery;
+mystery = "anything";
+mystery = 8;
+mystery = [9, "none", 83];
+mystery = true;
+const mysteryFunction = (value) => {
+    console.log(value);
+};
+let mysteryArray = [1, "two", 3, "four"];
+mysteryArray.push(5);
+mysteryArray.push("six");
+let tupleType = ["Years coding", 3];
 //# sourceMappingURL=index.js.map
