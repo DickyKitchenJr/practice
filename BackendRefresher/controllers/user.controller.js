1ec737707040e623ae8ee6cc9595e9b4f73e061d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2026-04-20 15:08:09 -0400

    Update user.controller.js
    added comments for additional clarity when referencing the code

--- BackendRefresher/controllers/user.controller.js
+++ BackendRefresher/controllers/user.controller.js
@@ -2,12 +2,14 @@ import { User } from "../models/user.model.js";
 
 const registerUser = async (req, res) => {
   try {
+    //establish what is to be expected from the request body
     const { username, password, email } = req.body;
 
     if (!username || !password || !email) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
+    //check if the username or email already exists in the database
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
       return res
@@ -15,19 +17,18 @@ const registerUser = async (req, res) => {
         .json({ message: "Username or email already exists" });
     }
 
+    //create a new user in the database
     const user = await User.create({
       username,
       password,
       email,
-      loggedIn: false,
+      loggedIn: false, //not currently in the schema, remove later?
     });
 
-    res
-      .status(201)
-      .json({
-        message: "User registered successfully",
-        user: { id: user._id, email: user.email, username: user.username },
-      });
+    res.status(201).json({
+      message: "User registered successfully",
+      user: { id: user._id, email: user.email, username: user.username },
+    });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ message: "Server error", error: error.message });
